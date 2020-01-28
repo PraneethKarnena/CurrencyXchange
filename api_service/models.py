f4e699b2ecccdc6cc73066f27c3d197e68d3f16a
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 08:46:55 +0530

    add Picture model

--- api_service/models.py
+++ api_service/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PictureModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
+    file = models.FileField(upload_to='profile-pictures/', null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.user.email} - {self.file.name}'
