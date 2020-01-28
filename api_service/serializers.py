140c88ac08c48988544015b27aca06b0e53408cd
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 22:43:40 +0530

    add file_url field

--- api_service/serializers.py
+++ api_service/serializers.py
@@ -5,6 +5,8 @@ from api_service import models
 
 class ProfilePictureSerializer(serializers.ModelSerializer):
 
+    file_url = serializers.ReadOnlyField(source='file.url')
+
     class Meta:
         model = models.PictureModel
-        fields = ['file']
+        fields = ['file', 'file_url']
