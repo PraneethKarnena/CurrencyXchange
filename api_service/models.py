d641a6cb7faec7d9d33f584899670dcc685c81ac
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 08:49:26 +0530

    add CurrencyConversion model

--- api_service/models.py
+++ api_service/models.py
@@ -16,3 +16,21 @@ class WalletModel(models.Model):
 
     def __str__(self):
         return f'{self.user.email} - {self.money}'
+
+
+class CurrencyConversionModel(models.Model):
+
+    from_currency = models.CharField(max_length=3, null=False, blank=False)
+    to_currency = models.CharField(max_length=3, null=False, blank=False)
+
+    from_amount = models.FloatField(default=1.00)
+    to_amount = models.FloatField(null=False, blank=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['from_currency', 'to_currency',]
+
+    def __str__(self):
+        return f'{self.from_currency} to {self.to_currency}: {self.to_amount}'
