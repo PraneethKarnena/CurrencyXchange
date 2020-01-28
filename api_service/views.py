7f923cf14f81ea45aa5559624eccc3c4fe035242
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 08:29:06 +0530

    implement sign up API view

--- api_service/views.py
+++ api_service/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup_api(request):
+    response_status = data = None
+    try:
+        email = request.data['email']
+        password = request.data['password']
+        confirm_password = request.data['confirm_password']
+
+        # Passwords do not match
+        if password != confirm_password:
+            raise Exception('Passwords do not match!')
+
+        user, created = User.objects.get_or_create(username=email, email=email)
+
+        # User already in the database
+        if not created:
+            raise Exception('User already exists!')
+
+        # Save the password to the database
+        user.set_password(password)
+        user.save()
+
+        data = {'success': True, 'message': 'User created successfully!'}
+        response_status = status.HTTP_200_OK
+
+    except Exception as e:
+        data = {'success': False, 'message': str(e)}
+        response_status = status.HTTP_400_BAD_REQUEST
+
+    return Response(data=data, status=response_status)
