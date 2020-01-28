80c2ec68f4656b71eca578c32233aada0dcd7b27
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 07:09:11 +0530

    implement sign up view

--- web_service/views.py
+++ web_service/views.py
@@ -11,4 +11,10 @@ def home_view(request):
 @require_http_methods(['GET'])
 def signin_view(request):
     """ Sign in page - return page with email and password fields """
-    return render(request, 'api_service/signin.html')
+    return render(request, 'web_service/signin.html')
+
+
+@require_http_methods(['GET'])
+def signup_view(request):
+    """ Sign up page - return page with email, confirm_password and password fields """
+    return render(request, 'web_service/signup.html')
