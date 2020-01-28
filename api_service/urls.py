d2780de65f67e7efc4b3aeef5959b109898dbabb
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 22:32:42 +0530

    add endpoint for profile pictures

--- api_service/urls.py
+++ api_service/urls.py
@@ -5,4 +5,6 @@ from api_service import views
 urlpatterns = [
     path('signup/', views.signup_api, name='signup_api'),
     path('signin/', views.signin_api, name='signin_api'),
+
+    path('profile-picture/', views.ProfilePictureListView.as_view(), name='profile_picture_api'),
 ]
