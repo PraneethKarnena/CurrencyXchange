9fdbc8076c2bdd1a206ab3126781d8be8aac824b
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 07:09:03 +0530

    add route for sign up

--- web_service/urls.py
+++ web_service/urls.py
@@ -6,4 +6,5 @@ from web_service import views
 urlpatterns = [
     path('', views.home_view, name='home_page'),
     path('signin/', views.signin_view, name='signin_page'),
+    path('signup/', views.signup_view, name='signup_page'),
 ]
