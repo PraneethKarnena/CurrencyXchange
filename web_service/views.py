6ad9c175948f829db71384faab128aa9e859b045
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-28 07:01:42 +0530

    implement home page

--- web_service/views.py
+++ web_service/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@require_http_methods(['GET'])
+def home_view(request):
+    """ Home page - return simple home template """
+    return render(request, 'web_service/home.html')
