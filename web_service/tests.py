48b7f72d6cb0f9b009a761c7a0662eefc1f3b12d
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-29 11:37:51 +0530

    add tests for web service

--- web_service/tests.py
+++ web_service/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class WebServiceTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('web_service:home_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signin_page_status_code(self):
+        response = self.client.get(reverse('web_service:signin_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_status_code(self):
+        response = self.client.get(reverse('web_service:signup_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_dashboard_page_status_code(self):
+        response = self.client.get(reverse('web_service:dashboard_page'))
+        self.assertEqual(response.status_code, 200)
